a9a77443c2d73a431a4a4753177648acdbc90b9a
Mayank <mayanks.rawat18@gmail.com>
2025-09-06 01:20:38 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -85,7 +85,7 @@ const Navbar = () => {
               animate={{ opacity: 1, x: 0 }}
               transition={{ delay: 0.2, duration: 0.5 }}
             >
-              UG.
+              MR.
             </motion.span>
           </NavLink>
 
